fa0f2f34de2fe5dd7db0075184576a619695ef2e
Subhamdas20 <subham21.sd@gmail.com>
2021-11-04 00:35:14 +0530

    Added : uc8 searched and inserted node

--- src/main/java/linkedlist/MyLinkedList.java
+++ src/main/java/linkedlist/MyLinkedList.java
@@ -1,6 +1,6 @@
 package linkedlist;
 
-public class MyLinkedList<T>  {
+public class MyLinkedList<T> {
     public INode tail;
     public INode head;
 
@@ -8,67 +8,91 @@ public class MyLinkedList<T>  {
         this.head = null;
         this.tail = null;
     }
+
     public void add(INode newNode) {
-        if(this.head == null){
+        if (this.head == null) {
             this.head = newNode;
         }
-        if(this.tail == null){
+        if (this.tail == null) {
             this.tail = newNode;
-        }
-        else{
+        } else {
             INode tempNode = this.head;
             this.head = newNode;
             this.head.setNext(tempNode);
         }
     }
+
     public void append(INode newNode) {
-        if(this.head == null){
+        if (this.head == null) {
             this.head = newNode;
         }
-        if(this.tail == null){
+        if (this.tail == null) {
+            this.tail = newNode;
+        } else {
+            this.tail.setNext(newNode);
             this.tail = newNode;
         }
-        else{
-            this.tail.setNext(newNode);
-            this.tail=newNode;
-        }
     }
-    public void insert(INode myNode, INode newNode){
+
+    public void insert(INode myNode, INode newNode) {
         INode tempNode = myNode.getNext();
         myNode.setNext(newNode);
         newNode.setNext(tempNode);
     }
-    public void pop(){
+
+    public void pop() {
         this.head = head.getNext();
     }
-    public <T>INode popLast(){
+
+    public <T> INode popLast() {
         INode tempNode = this.head;
-        while (!tempNode.getNext().equals(tail)){
+        while (!tempNode.getNext().equals(tail)) {
             tempNode = tempNode.getNext();
         }
         this.tail = tempNode;
         tempNode = tempNode.getNext();
         return tempNode;
     }
-    public  INode findNode(MyNode myNode){
+
+    public INode findNode(MyNode myNode) {
         INode tempNode = this.head;
         INode temp = null;
-        while (!tempNode.getNext().equals(tail)){
+        while (!tempNode.getNext().equals(tail)) {
 
-            if(tempNode.getNext()==myNode){
-                System.out.println("Found node with key : "+myNode.getKey());
-                temp=myNode;
+            if (tempNode.getNext() == myNode) {
+                System.out.println("Found node with key : " + myNode.getKey());
+                temp = myNode;
             }
             return temp;
         }
         return temp;
     }
-    public void printNodes(){
+
+    public INode searchAndInsert(MyNode myNode, INode keyToInsert) {
+        INode tempNode = this.head;
+        INode temp = null;
+        INode tempInsert = keyToInsert;
+        while (!tempNode.getNext().equals(tail)) {
+            if (tempNode.getNext() == myNode) {
+                System.out.println("Found node with key : " + myNode.getKey());
+                temp = tempNode;
+                break;
+            }
+        }
+        keyToInsert.setNext(temp.getNext().getNext());
+        temp.getNext().setNext(tempInsert);
+        tempInsert.setKey((keyToInsert.getKey()));
+        temp.getNext().setNext(keyToInsert);
+        return tempInsert;
+
+    }
+
+    public void printNodes() {
         StringBuffer myNodes = new StringBuffer("My Nodes : ");
-        INode tempNode = head ;
-        while (tempNode.getNext()!=null){
+        INode tempNode = head;
+        while (tempNode.getNext() != null) {
             myNodes.append(tempNode.getKey());
-            if(!tempNode.equals(tail)) myNodes.append("->");
+            if (!tempNode.equals(tail)) myNodes.append("->");
             tempNode = tempNode.getNext();
         }
         myNodes.append(tempNode.getKey());
